e49126dad194cf3ba7b71b42f780999c0a9e36d4
shrinidhikatti <chinnu.katti@gmail.com>
2026-01-25 17:19:11 +0530

    m2 branch

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatPrice(price: number): string {
   return new Intl.NumberFormat('en-IN', {
     style: 'currency',
-    currency: 'INR',
+    currency: 'INR', 
     minimumFractionDigits: 0,
   }).format(price)
 }
